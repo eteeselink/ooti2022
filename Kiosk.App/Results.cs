c0e6d364bfc9651064755950a032ddbc6600761b
Hassan Teymoori <hassan.teymoori1372@gmail.com>
2023-01-17 15:00:58 +0000

    create the format for return

--- Kiosk.App/Results.cs
+++ Kiosk.App/Results.cs
@@ -5,18 +5,31 @@ using System.Linq;
 using System.Collections.Generic;
 
 
+
 namespace Kiosk.App;
 
 class Results {
     public void Run() {
+        Console.WriteLine("TextResultReport");
+    }
+
+    public Dictionary<string, KeyValuePair<string, int>> proccessResults(Dictionary<string, List<String>> results) {
         var answers1 = new List<String>(){"T", "F", "T", "T", "T", "F"};
         var answers2 = new List<String>(){"Light Blue", "Green", "Red", "Brown", "Red"};
         var answers3 = new List<String>(){"True", "True", "False"};
-        var results = new Dictionary<string, List<String>>();
-        results.Add("Q1", answers1);
-        results.Add("Q2", answers2);
-        results.Add("Q3", answers3);
+        var fakeResults = new Dictionary<string, List<String>>();
+        fakeResults.Add("Q1", answers1);
+        fakeResults.Add("Q2", answers2);
+        fakeResults.Add("Q3", answers3);
+
+        var processedResults = new Dictionary<string, KeyValuePair<string, int>>();
 
 
+        foreach (var result in fakeResults)
+        {
+            Console.WriteLine("Daniel and hassan are here!");
+        }
+
+        return processedResults;
     }
 }
