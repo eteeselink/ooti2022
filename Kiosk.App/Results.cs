bcb2cf29e57f633f91a16eb691febd0f65c8a999
Mihai <mihaizelina@yahoo.com>
2023-01-17 15:43:35 +0000

    deleted commented code lines

--- Kiosk.App/Results.cs
+++ Kiosk.App/Results.cs
@@ -14,16 +14,7 @@ class Results {
     }
 
     public void proccessResults(Dictionary<string, List<String>> results) {
-        // var answers1 = new List<String>(){"T", "F", "T", "T", "T", "F"};
-        // var answers2 = new List<String>(){"Light Blue", "Green", "Red", "Brown", "Red"};
-        // var answers3 = new List<String>(){"True", "True", "False"};
-        // var fakeResults = new Dictionary<string, List<String>>();
-        // fakeResults.Add("Q1", answers1);
-        // fakeResults.Add("Q2", answers2);
-        // fakeResults.Add("Q3", answers3);
-
         var processedResults = new Dictionary<string, Dictionary<string, int>>();
-
 
         foreach (var question in results.Keys)
         {
