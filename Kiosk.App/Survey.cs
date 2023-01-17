c7646e62cf639672bc23e7ff3ef6d079bfe60475
Mihai <mihaizelina@yahoo.com>
2023-01-17 14:38:57 +0000

    initial mock functionality

--- Kiosk.App/Survey.cs
+++ Kiosk.App/Survey.cs
@@ -6,6 +6,32 @@ using System.Linq;
 namespace Kiosk.App;
 
 class Survey {
+
+    private Read reader;
+    private Results results;
+    private Dictionary<String, List<String>> questions;
+    private Dictionary<String, List<String>> answers;
+
+    private Dictionary<String, List<String>> MockDict() {
+        Dictionary<String, List<String>> mockQuestions = new Dictionary<String, List<String>>();
+
+        string[] answers = {"Y", "N"};
+        List<string> ansRange = new List<string>(answers);
+        mockQuestions.Add("Are cats better than dogs?", ansRange);
+
+        return mockQuestions;
+    }
+
+    private Dictionary<String, List<String>> MockAnswers() {
+        Dictionary<String, List<String>> mockAnswers = new Dictionary<String, List<String>>();
+
+        string[] answers = {"Y", "N", "Y", "Y"};
+        List<string> ansRange = new List<string>(answers);
+        mockAnswers.Add("Are cats better than dogs?", ansRange);
+
+        return mockAnswers;
+    }
+
     public void Run() {
         Console.WriteLine("Survey");
     }
